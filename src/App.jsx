cb1f46c45f7d9b970f073ac083e03bc7204f43b4
Patricio1Labra <patricio.labra1801@alumnos.ubiobio.cl>
2023-04-17 17:50:11 -0400

    cambios de codigo, intento de implementacion de hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import {Box,Card,CardContent,CardMedia,List,ListItem,TextField,IconButton,Backdrop,CircularProgress} from "@mui/material";
+import {Box,Card,CardContent,CardMedia,List,ListItem,TextField,IconButton,Backdrop,CircularProgress, ListSubheader} from "@mui/material";
 import Grid from '@mui/material/Unstable_Grid2'; 
 import ThumbDownIcon from '@mui/icons-material/ThumbDown';
 import FavoriteIcon from '@mui/icons-material/Favorite';
@@ -15,6 +15,8 @@ function App() {
   const [favoritos, setFavoritos] = useState([]);
   const [id, setId] = useState(0);
   const [open, setOpen] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+
   const handleClose = () => {
     setOpen(false);
   };
@@ -38,56 +40,74 @@ function App() {
         aleatoria += banco.charAt(Math.floor(Math.random() * banco.length));
     }
     setNombre(aleatoria);
-};
+  };
   
-const ObtenerDogNames = () => {
-    setNombre(dogNames.maleRandom());
-};
+  const ObtenerDogNames = () => {
+      setNombre(dogNames.maleRandom());
+  };
 
-const agregar = (perro) => {
-  setFavoritos([...favoritos, perro]);
-  console.log(id);
-  update();
-};
+  const agregar = (perro) => {
+    if(perro.lista==='aceptar'){
+      setFavoritos([...favoritos, perro]);
+    }
+    if(perro.lista==='rechazar'){
+      setRechazados([...rechazados, perro]);
+    }
+    update();
+  };
 
-const rechazar = (perro) => {
-  setRechazados([...rechazados, perro]);
-  console.log(id);
-  update();
-};
+  const borrar = (perro) => {
+    if(perro.lista==='aceptar'){
+      setFavoritos((favoritos) => favoritos.filter((item)=> item.id !== perro.id));
+    }
+    if(perro.lista==='rechazar'){
+      setRechazados((rechazados) => rechazados.filter((item)=> item.id !== perro.id));
+    }
+  };
 
-const borrarfavorito = (perro) => {
-  setFavoritos((favoritos) => favoritos.filter((item)=> item.id !== perro.id));
-};
+  const cambiar = (perro) =>{
+    if(perro.lista==='aceptar'){
+      borrar(perro);
+      perro.lista='rechazar';
+      agregar(perro)
+    }else{
+      borrar(perro);
+      perro.lista='aceptar';
+      agregar(perro)
+    }
+  };
 
-const rechazarfavorito = (perro) => {
-  setRechazados([...rechazados, perro]);
-  borrarfavorito(perro);
-};
+  const update = () => {
+    handleOpen();
+    ObtenerImagen();
+    //cadenaAleatoria();
+    ObtenerDogNames();
+    setId(id+1);
+  };
 
-const borrarrechazado = (perro) => {
-  setRechazados((rechazados) => rechazados.filter((item)=> item.id !== perro.id));
-};
+  const withLoading = async (fn) => {
+    try {
+      setIsLoading(true);
+      const result = await fn();
+      setIsLoading(false);
+      return result;
+    } catch (error) {
+      setIsLoading(false);
+      throw error;
+    }
+  };
 
-const agregarrechazado = (perro) => {
-  setFavoritos([...favoritos, perro]);
-  borrarrechazado(perro);
-};
-
-const update = () => {
-  handleOpen();
-  ObtenerImagen();
-  //cadenaAleatoria();
-  ObtenerDogNames();
-  setId(id+1);
-};
+  const handleClick = async () => {
+    const data = await withLoading(ObtenerImagen);
+    return data;
+  };
 
   useEffect(() => {
     update();
   }, []);
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box sx={{ flexGrow: 1 ,height:'100vh'}}>
       <Backdrop
         sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
         open={open}
@@ -95,10 +115,13 @@ const update = () => {
         <CircularProgress color="inherit" />
       </Backdrop>
       <Grid container spacing={2}>
-        <Grid xs={3} sx={{ background: "red" }}>
-          <List>
+        <Grid xs={3} sx={{ background: "red" ,paddingTop:"0"}}>
+          <List sx={{overflow:'auto',maxHeight:'95vh',paddingTop:'0'}}>
+            <ListSubheader>
+              Rechazados
+            </ListSubheader>
             {rechazados.map((item, index) => (
-              <ListItem key={index}>
+              <ListItem sx={{display:"block", paddingLeft:"0",paddingRight:"0",paddingTop:"0"}} key={index}>
                 <Card>
                   <CardMedia
                     component="img"
@@ -108,10 +131,10 @@ const update = () => {
                     {item.nombre}
                   </CardContent>
                   <Box sx={{ display: 'flex',justifyContent: 'space-evenly' }}>
-                    <IconButton aria-label="" disabled={open} onClick={() => borrarrechazado(item)}>
+                    <IconButton aria-label="" disabled={open} onClick={() => borrar(item)}>
                       <DeleteIcon sx={{ height: 38, width: 38 }} />
                     </IconButton>
-                    <IconButton aria-label="" disabled={open} onClick={() => agregarrechazado(item)}>
+                    <IconButton aria-label="" disabled={open} onClick={() => cambiar(item)}>
                       <FavoriteIcon sx={{ height: 38, width: 38 }} />
                     </IconButton>
                   </Box>
@@ -122,30 +145,32 @@ const update = () => {
         </Grid>
 
         <Grid xs={6} sx={{ background: "grey" }}>
-          <Card sx={{"max-height":'90vh'}}>
+          <Card>
             <CardMedia
               component="img"
-              object-fit= "contain"
               image={imagen}
             />
-            <CardContent sx={{display:'flex',justifyContent: 'center' }}>
+            <CardContent sx={{display:'flex',justifyContent: 'center',height:'1vh'}}>
               {nombre}
             </CardContent>
-            <Box sx={{ display: 'flex',justifyContent: 'space-evenly' }}>
-              <IconButton aria-label="" disabled={open} onClick={() => rechazar({imagen,nombre,id})}>
+            <Box sx={{ display:'flex',justifyContent:'space-evenly',height:'10vh'}}>
+              <IconButton aria-label="" disabled={open} onClick={() => agregar({imagen,nombre,id,lista:'rechazar'})}>
                 <ThumbDownIcon sx={{ height: 38, width: 38 }} />
               </IconButton>
-              <IconButton aria-label="" disabled={open} onClick={() => agregar({imagen,nombre,id})}>
+              <IconButton aria-label="" disabled={open} onClick={() => agregar({imagen,nombre,id,lista:'aceptar'})}>
                 <FavoriteIcon sx={{ height: 38, width: 38 }} />
               </IconButton>
             </Box>
           </Card>
         </Grid>
 
-        <Grid xs={3} sx={{ background: "green" }}>
-         <List>
+        <Grid xs={3} sx={{ background: "green" ,paddingTop:"0"}}>
+        <List sx={{overflow:'auto',maxHeight:'95vh',paddingTop:'0'}}>
+          <ListSubheader>
+            Aceptados
+          </ListSubheader>
             {favoritos.map((item, index) => (
-              <ListItem key={index}>
+              <ListItem sx={{display:"block", paddingLeft:"0",paddingRight:"0",paddingTop:"0"}} key={index}>
                 <Card>
                   <CardMedia
                     component="img"     
@@ -155,10 +180,10 @@ const update = () => {
                     {item.nombre}
                   </CardContent>
                   <Box sx={{ display: 'flex',justifyContent: 'space-evenly' }}>
-                    <IconButton aria-label="" disabled={open} onClick={() => borrarfavorito(item)}>
+                    <IconButton aria-label="" disabled={open} onClick={() => borrar(item)}>
                       <DeleteIcon sx={{ height: 38, width: 38 }} />
                     </IconButton>
-                    <IconButton aria-label="" disabled={open} onClick={() => rechazarfavorito(item)}>
+                    <IconButton aria-label="" disabled={open} onClick={() => cambiar(item)}>
                       <ThumbDownIcon sx={{ height: 38, width: 38 }} />
                     </IconButton>
                   </Box>
